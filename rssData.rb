8824f1140b32de27f3a539a41170549b02161d71
KSK-2001 <87355148+KSK-2001@users.noreply.github.com>
2022-06-02 01:44:07 +0530

    Create rssData.rb

---
+++ rssData.rb
@@ -0,0 +1,77 @@
+class RChannel
+
+    def Title=(t)
+      @Title = t
+    end
+    def Title
+      @Title      
+    end
+  
+    def Description=(t)
+      @Description = t
+    end
+    def Description
+      @Description      
+    end
+  
+    def Link=(t)
+      @Link = t
+    end
+    def Link
+      @Link      
+    end
+  
+    def PubDate=(t)
+      @PubDate = t
+    end
+    def PubDate
+      @PubDate      
+    end
+  
+    def RItems=(t)
+      @RItems = t
+    end
+    def RItems
+      @RItems      
+    end
+  
+  end
+  
+  class RItem
+    
+    def Title=(t)
+      @Title = t
+    end
+    def Title
+      @Title      
+    end
+    
+    def Description=(t)
+      @Description = t
+    end
+    def Description
+      @Description      
+    end
+  
+    def Link=(t)
+      @Link = t
+    end
+    def Link
+      @Link      
+    end
+  
+    def Guid=(t)
+      @Guid = t
+    end
+    def Guid
+      @Guid      
+    end
+  
+    def PubDate=(t)
+      @PubDate = t
+    end
+    def PubDate
+      @PubDate      
+    end
+  
+  end
